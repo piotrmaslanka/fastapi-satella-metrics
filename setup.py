243f764b794e66b418888c020b4ce420f2a22afb
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-07-04 18:22:19 +0200

    use nose2 to test

--- setup.py
+++ setup.py
@@ -8,9 +8,5 @@ setup(keywords=['fastapi', 'satella', 'metrics', 'instrumentation', 'prometheus'
       install_requires=[
             'satella', 'fastapi'
       ],
-      tests_require=[
-          "nose2", "mock", "coverage", "nose2[coverage_plugin]", "requests"
-      ],
-      test_suite='nose2.collector.collector',
-      python_requires='!=2.7.*,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
+      python_requires='!=2.7.*,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*',
       )
