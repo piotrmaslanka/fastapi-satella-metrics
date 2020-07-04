e1a4ef82336942fef36fdb4818ab3f1f109c1521
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-07-04 19:46:20 +0200

    fixed not exporting internal metrics

--- fastapi_satella_metrics/prometheus_exporter.py
+++ fastapi_satella_metrics/prometheus_exporter.py
@@ -19,8 +19,7 @@ def PrometheusExporter(app: fastapi.FastAPI,
             if not datum.internal:
                 new_values.add(datum)
         metric_data.values = new_values
-        if labels is not None:
-            metric_data.add_labels(labels)
+        metric_data.add_labels(labels)
         return fastapi.Response(content=metric_data_collection_to_prometheus(metric_data),
                                 media_type='text/plain')
 
